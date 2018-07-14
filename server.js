c17c1856deff9b15f93349a8100c91fa872c3f08
Rizal Ibnu <muhamadrizalibnuabdulah@gmail.com>
2018-07-14 22:31:24 +0700

    Initial commit json placeholder

---
+++ server.js
@@ -0,0 +1,30 @@
+const jsonServer = require('json-server');
+const uncached = require('require-uncached');
+const server = jsonServer.create();
+const router = jsonServer.router('data.json');
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 4000;
+
+server.get('/refresh', (req, res) => {
+  router.db.assign(uncached('./data.json')).write();
+  res.sendStatus(200);
+});
+
+// Set default middlewares (logger, static, cors and no-cache)
+server.use(middlewares);
+
+// To handle POST, PUT and PATCH you need to use a body-parser
+// You can use the one used by JSON Server
+server.use(jsonServer.bodyParser)
+server.use((req, res, next) => {
+  if (req.method === 'POST') {
+    req.body.createdAt = Date.now()
+  }
+  // Continue to JSON Server router
+  next()
+})
+
+// Use default router
+server.use(router);
+
+server.listen(port);
---
+++ start.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+imageName=rizalibnu/jsonplaceholder
+containerName=jsonplaceholder
+
+echo Building image...
+time docker build -t $imageName -f Dockerfile  .
+
+echo Delete old container...
+docker rm -f $containerName
+
+echo Run new container...
+docker-compose up -d
+
+echo Prune images...
+docker image prune -f
